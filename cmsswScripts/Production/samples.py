0945364ba8ed32f72dfc8f0ae929c5d4c2657728
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-03-28 11:32:42 +0100

    lumiMask for data added

--- cmsswScripts/Production/samples.py
+++ cmsswScripts/Production/samples.py
@@ -1,4 +1,4 @@
-import ROOT
+import ROOT,os
 files = {
   "MultiJet__Run2012A-22Jan2013-v1__AOD": {
     "localFile": ['/store/data/Run2012A/MultiJet/AOD/22Jan2013-v1/20000/0036C47E-0B74-E211-B992-00266CF32684.root']    ,"backupLocalFile":"/user/hoehle/CMSSW/TEMP/Samples/0051A2ED-482A-E111-9863-0026189438F7.root", "label":"MultiJetData"
@@ -7,9 +7,11 @@ files = {
     ,"color":ROOT.kBlack+1
     ,"addOptions":"runOnData=True"
     ,"crabConfig":{
-          "CMSSW":{"lumis_per_job":5,"total_number_of_lumis" : 100}
+          "CMSSW":{"lumis_per_job":5,"total_number_of_lumis" : -1,"lumi_mask":os.getenv('CMSSW_BASE')+'/FWLitePython/cmsswScripts/Production/jsonFiles/'+'GoldenJSON_MultiJetsRunA2012_JSON.txt'}
           #"CMSSW":{"number_of_jobs":1500}
           #"GRID":{"se_white_list":"T2_DE_RWTH"}
+          #https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions12/8TeV/Reprocessing/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt
+          ,'USER':{"publish_data": 1,"publish_data_name" : "BScSkim_MultiJetData"}
     }
   }
   ,"TTTT_TuneZ2star_8TeV-madgraph-tauola__Summer12_DR53X-PU_S10_START53_V7A-v1__AODSIM":{
