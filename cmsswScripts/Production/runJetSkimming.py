0783a88b3007e03f2cf28a8a664632c3bc8e9b6e
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-05-21 13:51:25 +0200

    added ttbar skim

--- cmsswScripts/Production/runJetSkimming.py
+++ cmsswScripts/Production/runJetSkimming.py
@@ -8,8 +8,8 @@ import  cmsswAnalysisTools
 import samples
 print "executing "," ".join(sys.argv)
 cfg = os.getenv('CMSSW_BASE')+'/FWLitePython/cmsswScripts/softJetSkim_cfg.py'
-if not 'SingleMu__hoehle-BScSkim_SingleMuData-bd22b76fe02f0bcf6d005e3433b8e12b__USER' in sys.argv:
-  print "use --specificSamples SingleMu__hoehle-BScSkim_SingleMuData-bd22b76fe02f0bcf6d005e3433b8e12b__USER for this script"
+if not 'SingleMu__hoehle-BScSkim_SingleMuData-bd22b76fe02f0bcf6d005e3433b8e12b__USER' in sys.argv and not 'TTJets_TuneZ2star_8TeV-madgraph-tauola__fhohle-BScSkim_TTJets-10916883f87702ed6d1a4822bbfe7433__USER' in sys.argv:
+  print "use --specificSamples SingleMu__hoehle-BScSkim_SingleMuData-bd22b76fe02f0bcf6d005e3433b8e12b__USER or TTJets_TuneZ2star_8TeV-madgraph-tauola__fhohle-BScSkim_TTJets-10916883f87702ed6d1a4822bbfe7433__USER for this script"
   sys.exit(1)
 myAnalysis = cmsswAnalysisTools.cmsswAnalysis(samples.files,cfg)
 myAnalysis.readOpts()
