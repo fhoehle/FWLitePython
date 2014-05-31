53c267b70b7e5eb237e87000149f4f767f578306
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-05-31 08:34:52 +0200

    added trigger + jet skimming

---
+++ cmsswScripts/Production/runPFPAT_triggerAndJetSkimming.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# run cmssw analysis
+import FWCore.ParameterSet.Config as cms
+import os
+import sys
+sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/Tools')
+import  cmsswAnalysisTools
+import samples
+print "executing "," ".join(sys.argv)
+cfg = os.getenv('CMSSW_BASE')+'/FWLitePython/cmsswScripts/runPATwithPFBReco_andTriggerJetSkim.py'
+myAnalysis = cmsswAnalysisTools.cmsswAnalysis(samples.files,cfg)
+if "SingleMu__Run2012A-22Jan2013-v1__AOD" in sys.argv or "SingleMu__Run2012B-22Jan2013-v1__AOD":
+  sys.argv.append('--runOnData')
+myAnalysis.readOpts()
+myAnalysis.startAnalysis()
+
